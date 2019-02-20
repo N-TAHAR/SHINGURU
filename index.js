c7a2d7283512ca3827916fe6788e35185b6d669f
N.TAHAR <najibtaharberrabah@gmail.com>
2019-02-20 20:39:17 +0100

    home page button links

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 let header = document.querySelector('.header');
 
-
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', ()=>{
   if(window.scrollY > 0){
     header.classList.add('is-scrolled');
   }else{
     header.classList.remove('is-scrolled');
   }
 })
+
+// let back = document.querySelector('back');
+
+// back.addEventListener('click', ()=>{
+  
+// })
